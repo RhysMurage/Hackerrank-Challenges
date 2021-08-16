be98e511f1af9c4effd8a6a7ba11c2c75f8dabf5
RhysMurage <rhysmwangi96@gmail.com>
2021-08-16 16:42:55 +0300

    all tests are now ok

--- beginner.py
+++ beginner.py
@@ -296,10 +296,10 @@
 
 # String Validators
 s = input()
-
+i=0
+x=len(s)
+#alphanumerical
 for letter in s:
-    i = 0
-    x = len(s)-1
     while i <= x:
         letter = s[i]
         if letter.isalnum():
@@ -316,10 +316,10 @@ for letter in s:
             break
     break
 
+#alphabetical
 for letter in s:
-    i = 0
-    x = len(s)-1
     while i <= x:
+        letter = s[i]
         if letter.isalpha():
             print('True')
             break
@@ -330,10 +330,45 @@ for letter in s:
             i += 1
     break
 
-    
+#digits
+for letter in s:
+    while i <= x:
+        letter = s[i]
+        if letter.isdigit():
+            print('True')
+            break
+        elif not letter.isdigit():
+            if i == x:
+                print('False')
+                break
+            i += 1
+    break
 
-    
-            
-      
+#lowercase
+for letter in s:
+    while i <= x:
+        letter = s[i]
+        if letter.islower():
+            print('True')
+            break
+        elif not letter.islower():
+            if i == x:
+                print('False')
+                break
+            i += 1
+    break           
 
+#uppercase     
+for letter in s:
+    while i <= x:
+        letter = s[i]
+        if letter.isupper():
+            print('True')
+            break
+        elif not letter.isupper():
+            if i == x:
+                print('False')
+                break
+            i += 1
+    break
 
