cf03121a8736680bb91443c3a20914d542243658
RhysMurage <rhysmwangi96@gmail.com>
2021-09-01 16:09:32 +0300

    Text wrap with textwarp module

--- beginner.py
+++ beginner.py
@@ -387,26 +387,38 @@
 # print(any(c.islower() for c in s))
 # print(any(c.isupper() for c in s))
 
-# Text alignment
-thickness = int(input()) #This must be an odd number
-c = 'H'
+# # Text alignment
+# thickness = int(input()) #This must be an odd number
+# c = 'H'
 
-#Top Cone
-for i in range(thickness):
-    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+# #Top Cone
+# for i in range(thickness):
+#     print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
 
-#Top Pillars
-for i in range(thickness+1):
-    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+# #Top Pillars
+# for i in range(thickness+1):
+#     print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
 
-#Middle Belt
-for i in range((thickness+1)//2):
-    print((c*thickness*5).center(thickness*6))    
+# #Middle Belt
+# for i in range((thickness+1)//2):
+#     print((c*thickness*5).center(thickness*6))    
 
-#Bottom Pillars
-for i in range(thickness+1):
-    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+# #Bottom Pillars
+# for i in range(thickness+1):
+#     print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
 
-#Bottom Cone
-for i in range(thickness):
-    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+# #Bottom Cone
+# for i in range(thickness):
+#     print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+# Text Wrap
+
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+
+
+string, max_width = input(), int(input())
+result = wrap(string, max_width)
+print(result)
